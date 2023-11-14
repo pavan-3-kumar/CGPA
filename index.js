69acaffbcf3d0173db8a8be60afcea3cb1ee60cb
Pavan_THE01~ <vpavankumar2003@gmail.com>
2023-11-14 21:47:03 +0530

    after adding cgpa also

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ let credits = [[[3, 3, 3, 3, 1.5, 1.5, 3, 1.5], [3, 3, 3, 3, 2, 1.5, 1, 1.5, 1.5
 let subjects = [[['M1', 'EP', 'BEEE', 'PPS', 'EP LAB', 'BEEE LAB', 'EG LAB', 'PPS LAB'], ['M2', 'EC', 'DE', 'ECS', 'PP', 'EC LAB', 'PP LAB', 'CEW', 'ECS LAB']], [['P&S', 'DMS', 'CO', 'DS', 'OOP', 'P&S LAB', 'DS LAB', 'OOP LAB', 'SOC'], ['CS', 'DBMS', 'OS', 'SE', 'WT', 'CS LAB', 'DBMS LAB', 'WT LAB', 'SOC']], [['AFL', 'CN', 'DAA', 'PE-1', 'OE-1', 'DAA LAB', 'DA LAB', 'SUM INTERN', 'SOC'], ['AI', 'CNS', 'ML', 'PE-2', 'OE-2', 'AI LAB', 'ML LAB', 'TERM PAPER', 'SOC']], [['HSS', 'PE-3', 'PE-4', 'PE-5', 'OE-3', 'OE-4', 'INTERN', 'SOC'], ['PROJECT/INTERN']]];
 let grades={'A+':10,'A':9,'B':8,'C':7,'D':6,'E':5};
 let year  , sem ; 
+let sgpa =0  ;
 app.post('/details', function(req , res)
 {
    // retriveing the selected year and semester data 
@@ -30,7 +31,8 @@ app.post('/details', function(req , res)
 app.post('/result' , function(req , res)
 {
    // calculating the sgpa 
-   let total = 0 ,sum =0 ;
+   let sum =0 ;
+   sgpa = 0 ;
    const sub = subjects[year][sem];
    // taking all the data of the form i.e all the subjects points
    // this is an object 
@@ -38,17 +40,52 @@ app.post('/result' , function(req , res)
    let i=0 ; 
    for(const [subj, gpa] of Object.entries(pa))
    {
-       total += gpa*credits[year][sem][i];
+       sgpa += gpa*credits[year][sem][i];
        sum += credits[year][sem][i];
        i++;
    }
-   total /= sum;
-   total = Math.round(total * 100)/100;
+   sgpa /= sum;
+   sgpa = Math.round(sgpa * 100)/100;
+   
+   // console.log(sgpa);
    // console.log(total);
    // showing the result
-   res.render('print_res' , {result:total});
+   res.render('print_res' , {result:sgpa});
    // res.redirect('/');
 });
+var cal_cgpa =[['1-1' ,'1-2'] ,['2-1','2-2'],['3-1','3-2'],['4-1','4-2']];
+
+app.post('/cgpa' , function(req ,res){
+   if(year === 0 && sem === 0)
+   {   
+      res.render('final',{final_result:sgpa});
+   }
+   else {
+ 
+   res.render('cgpa',{year:year  , semester : sem , sems:cal_cgpa });}
+});
+let  cgpa =0 ;
+app.post('/cal_total' , function(req , res){
+   const pa = req.body;
+   // console.log(pa);
+   cgpa =0 ;
+   for(const [ye , value] of Object.entries(pa))
+   {
+      let ans = parseFloat(value);
+      // console.log(ans);
+      cgpa += ans;
+   }
+   // console.log(sgpa);
+   cgpa += sgpa;
+   let count_sems = (year) * 2 + sem+1;
+
+   console.log(count_sems);
+   cgpa = cgpa / count_sems;
+   cgpa = Math.round(cgpa * 100)/100;
+   console.log(cgpa);
+   res.render('final',{final_result:cgpa});
+});
+
 app.listen(port , function(err)
 {
    if(err){console.log("there is an error");return ;}
